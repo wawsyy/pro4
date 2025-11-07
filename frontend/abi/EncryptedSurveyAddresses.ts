6274bb59da3044efcf5fd0ef7400633807cb5ed5
wswsyy <shiyu689@qq.com>
2025-11-07 01:30:00 +0000

    chore: update sepolia address for encrypted survey

--- frontend/abi/EncryptedSurveyAddresses.ts
+++ frontend/abi/EncryptedSurveyAddresses.ts
@@ -4,6 +4,6 @@
   Command: 'npm run genabi'
 */
 export const EncryptedSurveyAddresses = { 
-  "11155111": { address: "0x0000000000000000000000000000000000000000", chainId: 11155111, chainName: "sepolia" },
+  "11155111": { address: "0xd838319c0A79721Cc801220d57b1d9BbC557b59a", chainId: 11155111, chainName: "sepolia" },
   "31337": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 31337, chainName: "localhost" },
 };
